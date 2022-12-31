fb30e2ee5d6ec8abc85a9182bde5a7097364e882
Theodore Redlon <redlontheo@gmail.com>
2022-12-31 08:16:21 -0500

    added variables for repeating code in Rook

--- Pieces/Rook.py
+++ Pieces/Rook.py
@@ -9,33 +9,39 @@ class Rook(Piece):
         Piece.__init__(self, team)
 
     def move(self, board, current_pos, new_pos):
+        new_row = new_pos[0]
+        new_col = new_pos[1]
+        current_row = current_pos[0]
+        current_col = current_pos[1]
+
         # Vertical Change
-        if new_pos[1] == current_pos[1]:
+        if new_col == current_col:
 
             # Up or Down
-            route = 1 if new_pos[0] > current_pos[0] else -1
-            for i in range(current_pos[0] + route, new_pos[0] + route, route):
-                if board[i][new_pos[1]] != None:
-                    if board[i][new_pos[1]].team is self.team:
+            route = 1 if new_row > current_row else -1
+            for i in range(current_row + route, new_row + route, route):
+                if board[i][new_col] != None:
+                    if board[i][new_col].team is self.team:
                         print("Same team!")
                         return False
-                    if i != new_pos[0]:
+                    if i != new_row:
                         print("Piece in the way")
                         return False
                     return True
             return True
 
         # Horizontal Change
-        if new_pos[0] == current_pos[0]:
+        if new_row == current_row:
+
             # Up or Down
-            route = 1 if new_pos[1] > current_pos[1] else -1
-            for i in range(current_pos[1] + route, new_pos[1] + route, route):
-                if board[new_pos[0]][i] != None:
-                    if board[new_pos[0]][i].team is self.team:
+            route = 1 if new_col > current_col else -1
+            for i in range(current_col + route, new_col + route, route):
+                if board[new_row][i] != None:
+                    if board[new_row][i].team is self.team:
                         print("Same Team!")
                         return False
                     else:
-                        if i != new_pos[1]:
+                        if i != new_col:
                             print("Piece in the way")
                             return False
                     return True
